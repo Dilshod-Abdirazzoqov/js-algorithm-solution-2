68d9afa4fe337a8ae36bb9d510e9508d61fd4bdd
Dilshod <example@gmail.com>
2022-01-28 16:53:25 +0500

    JS-Exercise

--- script.js
+++ script.js
@@ -1,37 +1,85 @@
-//Masala
-// 1) o'zgaruvchan numberOfNews yarating va foydalanuvchidan savolga javobini qo'ying:
-// "Siz qancha yangilik ko'rdingiz?"
-// 2) personalNewsDB obyektini yarating va unga quyidagi xususiyatlarni qo'ying:
-// - count - birinchi savolga javob shu erda beriladi
-// - news - bu ob'ektga bo'sh ob'ektni qo'ying
-// - actors - shuningdek, bo'sh ob'ekt joylashtiradilar
-// - genres - bu erda bo'sh massivni qo'ying
-// - privat - bu xususiyatga mantiqiy (boolean) false qiymatini qo'ying
-// 3) foydalanuvchiga ikkita savol bering:
-//   - "Oxirgi ko'rgan yangiliklarizdan biri?"
-//   - 'Unga qancha baho bergan bo'lar edingiz?'
-// Javoblar alohida o'zgaruvchilarga joylashtirilishi kerak.
-// Yangiliklar ob'ektiga javoblarni quyidagi shaklda yozing:
-//   news: {
-//     'BBC': '8.1'
-//   }
+/* Masala:
+1) Tsiklni yordamida foydalanuvchiga yangiliklar haqidagi savollarni avtomatlashtirish
+2) Foydalanuvchi javobni bo'sh satr sifatida qoldirmasligi uchun shunday qiling,
+javobni bekor qilish yoki 50 belgidan uzunroq yangilik nomini kiritish. Agar bu sodir bo'lsa -
+foydalanuvchini yana savollarga qaytaring. (Har qanday satrga quyidagicha kirish mumkin
+str.length - va uzunligini oling)
+3) Shartlardan foydalanib, personalMovieDB.count-ni tekshiring, agar u 10 dan kam bo'lsa - xabarni ko'rsating
+"Juda oz sonli yangiliklar o'qilibdi", agar 10 dan 30 gacha bo'lsa - "Siz klassik tomoshabinsiz", agar ko'proq bo'lsa -
+- Siz yangilklar ishqibozisiz. Va agar u biron bir variantga mos kelmasa - "Xato yuz berdi"
+4) Amaliy mashg'ulot va tsiklni yana ikkita usulda qayta yozing  */
 
 "use strict";
+// One Way
+// const numberOfNews = +prompt("Siz nechta yangilik ko'rdingiz?");
+// --------------------------------------
+//First-step
+// do {
+//   prompt("Siz nechta yangilik ko'rdingiz?");
+//   prompt("Oxirgi ko'rgan yangiliklarizdan biri?");
+//   prompt("Unga qancha baho bergan bo'lar edingiz?");
+// } while (numberOfNews == undefined || numberOfNews > 50);
+//---------------------------------------
+// const personalNewsDB = {
+//   count: numberOfNews,
+//   //create object called news;
+//   news: {},
+//   actors: {},
+//   //create array called genres;
+//   genres: [],
+//   privat: false,
+// };
+//----------------------------------------
+//Third-step
+// if (personalNewsDB.count < 10) {
+//   console.log("Juda oz sonli yangiliklar o'qilibdi");
+// } else if (personalNewsDB.count > 10 && personalNewsDB.count < 30) {
+//   console.log("Siz klassik tomoshabinsiz");
+// } else if (personalNewsDB.count > 30) {
+//   console.log("Siz yangilklar ishqibozisiz");
+// } else {
+//   console.log("Xato yuz berdi");
+// }
+//------------------------------------------
+// const a = prompt("Oxirgi ko'rgan yangiliklarizdan biri?"),
+//   b = prompt("Unga qancha baho bergan bo'lar edingiz?");
+//------------------------------------------
+// personalNewsDB.news[a] = b;
+// console.log(personalNewsDB);
 
+//------------------------------------------
+//Second Way;
 const numberOfNews = +prompt("Siz qancha yangilik ko'rdingiz?");
 
 const personalNewsDB = {
   count: numberOfNews,
-  //create object called news;
   news: {},
   actors: {},
-  //create array called genres;
   genres: [],
   privat: false,
 };
 
-const a = prompt("Oxirgi ko'rgan yangiliklarizdan biri?"),
-  b = prompt("Unga qancha baho bergan bo'lar edingiz?");
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Oxirgi ko'rgan yangiliklarizdan biri?"),
+    b = prompt("Unga qanday baho bergan bo'lar edingiz?");
 
-personalNewsDB.news[a] = b;
+  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+    personalNewsDB.news[a] = b;
+    console.log("tayyor");
+  } else {
+    console.log("Error");
+    i--;
+  }
+}
+
+if (personalNewsDB.count < 10) {
+  console.log("Juda oz sonli yangiliklar o'qilibdi");
+} else if (personalNewsDB.count >= 10 && personalNewsDB.count < 30) {
+  console.log("Siz klassik tomoshabinsiz");
+} else if (personalNewsDB.count >= 30) {
+  console.log("Siz yangilklar ishqibozisiz");
+} else {
+  console.log("Xato yuz berdi");
+}
+
 console.log(personalNewsDB);
